0b705634a668cfd8f3deeb6367d873f9365ee8c3
Robert Beekman <robert@matsimitsu.nl>
2013-11-07 23:41:59 +0100

    add arduino

--- monitor.rb
+++ monitor.rb
@@ -1,6 +1,19 @@
+require 'em-serialport'
+require 'json'
+
 EM.run do
+  str = ''
   serial = EventMachine.open_serial('/dev/ttyACM0', 9600, 8, 1, 0)
   serial.on_data do |data|
-    puts data.inspect
+    str << data
+    if str.split('').last == "\n"
+      puts str
+      begin
+        puts JSON.parse(str)
+      rescue => e
+        puts e.inspect
+      end
+      str = ''
+    end
   end
 end
