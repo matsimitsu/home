d056af0f2754bb86883c47e6858858bbef7bedcf
Robert Beekman <robert@matsimitsu.nl>
2013-11-09 18:59:03 +0100

    Very basic view

--- app.rb
+++ app.rb
@@ -8,3 +8,23 @@ end
 set :database, "sqlite3:///measurements.db"
 
 require_folder("models")
+
+get "/" do
+  @electricity = Measurement.electricity.last_five_minutes.count
+  @gas = Measurement.gas.last_five_minutes.count
+  @water = Measurement.water.last_five_minutes.count
+
+  haml :index
+end
+
+
+
+helpers do
+  def format_electricity(number)
+    (number.to_f / 1000).round(2)
+  end
+
+  def format_gas(number)
+    (number.to_f / 100).round(2)
+  end
+end
--- models/measurement.rb
+++ models/measurement.rb
@@ -1,3 +1,10 @@
 class Measurement < ActiveRecord::Base
 
+  scope :last_five_minutes, -> {
+    where("created_at  > ?", 5.minutes.ago)
+  }
+
+  scope :electricity, -> { where(:kind => 'e') }
+  scope :gas, -> { where(:kind => 'g') }
+  scope :water, -> { where(:kind => 'w') }
 end
