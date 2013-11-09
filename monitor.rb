1769abcc039b3da741f5999f0e9417676d64d494
Robert Beekman <robert@matsimitsu.nl>
2013-11-09 17:23:20 +0100

    Use string for kind

--- monitor.rb
+++ monitor.rb
@@ -9,7 +9,7 @@ EM.run do
   str = ''
   serial = EventMachine.open_serial('/dev/ttyACM0', 9600, 8, 1, 0)
   serial.on_data do |data|
-    kind = /[a-z]/.match(data)
+    kind = /[a-z]/.match(data).to_s
     if kind
       Measurement.create(:kind => kind)
       puts kind
